883bef61941b3a050b577984cb631d7282d3664b
tony <tantony.24.ichat.sp.edu.sg>
2024-05-04 00:11:56 +0800

    FIle CHANGES

--- Test_BE.py
+++ Test_BE.py
@@ -1,19 +1,21 @@
 import Lab2.BE as test
 import statistics
 
-test_temps = list[12,23,25,10,5,15,20]
 
 def Test_find_min_max():
+   test_temps = [12,23,25,10,5,15,20]
    result = test.find_min_max(test_temps)
    
    assert (result == min(test_temps) & result == max(test_temps))
 
 def Test_calc_average():
+   test_temps = [12,23,25,10,5,15,20]
    result = test.calc_average(test_temps)
 
    assert (result == (sum(test_temps) / len(test_temps)))
 
 def Test_calc_median_temperature():
+   test_temps = [12,23,25,10,5,15,20]
    result = test.calc_median_temperature(test_temps)
 
    assert (result == statistics.median(test_temps) )
